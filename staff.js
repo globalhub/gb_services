9d2b4c7eb3bbaddca16c3322d6cc7657b37db1d3
yemigabriel <yemigabriel@gmail.com>
2015-01-31 14:59:16 +0100

    c

--- staff.js
+++ staff.js
@@ -1,10 +1,5 @@
-//var serviceURL = "http://localhost/ronmobile/services/";
-var serviceURL = "http://localhost/mbjson/";
-//var serviceURL = "http://muveebound.com/mapp/services/mbjson/";
-//var serviceURL = "http://166.78.185.95/doxa360/RON/services/";
 var serviceURL =  "http://localhost/gb_services/";
 var members;
-var fbkemail;
 
 function allstaffmembers() {
 	$.mobile.pageloading();
@@ -29,5 +24,5 @@ function allstaffmembers() {
 $('#home').live('pageshow', function(event) {
 	
 	allstaffmembers();
-	
+
 });
